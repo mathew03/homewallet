59f4467fa50e1b8ebb38d137fb7da3da45cea05c
BOROWSKI Mateusz | DPS Software <MBorowski@cns.internal>
2017-10-23 17:12:05 +0200

    MainForm

--- HomeWallet/HomeWallet/Form1.Designer.cs
+++ HomeWallet/HomeWallet/Form1.Designer.cs
@@ -1,6 +1,8 @@
-﻿namespace HomeWallet
+﻿using System.Drawing;
+
+namespace HomeWallet
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +30,100 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel_sideMenu = new System.Windows.Forms.Panel();
+            this.button_dashboard = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel_sideMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 200F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.panel_sideMenu, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(874, 521);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // panel_sideMenu
+            // 
+            this.panel_sideMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(34)))), ((int)(((byte)(34)))));
+            this.panel_sideMenu.Controls.Add(this.button2);
+            this.panel_sideMenu.Controls.Add(this.button1);
+            this.panel_sideMenu.Controls.Add(this.button_dashboard);
+            this.panel_sideMenu.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel_sideMenu.Location = new System.Drawing.Point(0, 0);
+            this.panel_sideMenu.Margin = new System.Windows.Forms.Padding(0);
+            this.panel_sideMenu.Name = "panel_sideMenu";
+            this.panel_sideMenu.Size = new System.Drawing.Size(200, 521);
+            this.panel_sideMenu.TabIndex = 0;
+            // 
+            // button_dashboard
+            // 
+            this.button_dashboard.FlatAppearance.BorderSize = 0;
+            this.button_dashboard.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_dashboard.ForeColor = System.Drawing.Color.White;
+            this.button_dashboard.Location = new System.Drawing.Point(3, 9);
+            this.button_dashboard.Name = "button_dashboard";
+            this.button_dashboard.Size = new System.Drawing.Size(188, 44);
+            this.button_dashboard.TabIndex = 0;
+            this.button_dashboard.Text = "Panel główny";
+            this.button_dashboard.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(3, 59);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(188, 44);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Użytkownicy";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(3, 109);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(188, 44);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Kategorie";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.ClientSize = new System.Drawing.Size(874, 521);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "MainForm";
+            this.Text = "Home wallet";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel_sideMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel_sideMenu;
+        private System.Windows.Forms.Button button_dashboard;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
